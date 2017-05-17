b3948d099d9cf8c8ec5fb4f2d91bfc444a20616a
Angel Piscola <angelpiscola@gmail.com>
2017-05-17 01:14:23 -0400

    chore: add next to global npm packages installed

--- vagrant/scripts/install_nvm.sh
+++ vagrant/scripts/install_nvm.sh
@@ -11,7 +11,8 @@ export NODE_VERSION='--lts=boron'
 nvm install $NODE_VERSION
 
 # Install global packages
-# npm install -g yarn
+npm install -g next
+
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
 
