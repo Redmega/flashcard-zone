90b799c0538daeacd58561099f8c9a6df408d4b6
Angel Piscola <angelpiscola@gmail.com>
2017-05-25 23:55:25 -0400

    feat: upgrade apollo to 0.4.0

--- server/index.js
+++ server/index.js
@@ -1,21 +1,46 @@
 import express from 'express';
-import { apolloServer } from 'apollo-server';
-import schema from '../data/schema.graphql';
+import bodyParser from 'body-parser';
+
+import { graphqlExpress, graphiqlExpress } from 'graphql-server-express';
+import { makeExecutableSchema, addMockFunctionsToSchema } from 'graphql-tools';
+
+import Schema from '../data/schema.graphql';
 import Mocks from '../data/mocks';
+
 import config from './config';
 
 const { GRAPHQL_PORT } = config;
 const server = express();
 
+const executableSchema = makeExecutableSchema({
+  typeDefs: Schema,
+  // resolvers: Resolvers,
+  // connectors: Connectors,
+});
+
+addMockFunctionsToSchema({
+  schema: executableSchema,
+  mocks: Mocks,
+  preserveResolvers: true,
+});
+
 server.use(
   '/graphql',
-  apolloServer({
-    graphiql: true,
+  bodyParser.json(),
+  graphqlExpress({
+    schema: executableSchema,
+    context: {}, //at least(!) an empty object
     pretty: true,
-    schema: [schema],
-    mocks: Mocks,
   }),
 );
+
+server.use(
+  '/graphiql',
+  graphiqlExpress({
+    endpointURL: '/graphql',
+  }),
+);
+
 server.listen(GRAPHQL_PORT || 3000, () =>
   console.log(`GraphQL Server is now running on http://localhost:${GRAPHQL_PORT}/graphql`),
 );
