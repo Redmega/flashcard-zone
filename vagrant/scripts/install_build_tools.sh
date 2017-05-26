f81ba27c9a7db5a2384351a82ef9f39e66a73010
Angel Piscola <angelpiscola@gmail.com>
2017-05-25 23:33:33 -0400

    chore: remove invalid glibc apt-get

--- vagrant/scripts/install_build_tools.sh
+++ vagrant/scripts/install_build_tools.sh
@@ -7,4 +7,4 @@ apt-get upgrade -y
 
 # apt-get install -y make g++ libcairo2-dev libav-tools
 export DEBIAN_FRONTEND=noninteractive
-apt-get install -y build-essential git curl wget vim nfs-common portmap htop ca-certificates glibc libc6
+apt-get install -y build-essential git curl wget vim nfs-common portmap htop ca-certificates libc6
